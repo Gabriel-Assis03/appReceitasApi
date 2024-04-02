373fd0cfb77c8c68c976b5257cc067f325e572d6
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2024-04-02 17:12:10 -0300

    r8

--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -48,7 +48,15 @@ public class UserController : ControllerBase
     [HttpPut("{email}")]
     public IActionResult Update(string email, [FromBody]User user)
     {
-        throw new NotImplementedException();
+        try
+        {
+            _service.UpdateUser(user);
+        }
+        catch (System.Exception)
+        {
+            return BadRequest();
+        }
+        return Ok();
     }
 
     // 9 - Sua aplicação deve ter o endpoint DEL /user
